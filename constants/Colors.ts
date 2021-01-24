1e8d3edc040e3448ca6e0065c28759226d1c3c24
heatherbeck <72701201+heatherbeck@users.noreply.github.com>
2021-01-24 11:40:50 -0600

    Add files via upload

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#D0021B';
+
+export default {
+  light: {
+    text: '#417505000',
+    background: '#D0021B',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#417505',
+    background: '#D0021B',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
